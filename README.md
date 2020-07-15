792beffe34c7a30e09f74bf53f736b92bf730868
Rotem Levi <rotem.levi@ironsrc.com>
2020-07-15 22:57:10 +0300

    stable version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vscode-remote-docker-typescript
-Test project to create a skeleton app for node.js in typescript on a docker container with visual studio code
+
+Test project to create a skeleton app for node.js in typescript on a docker container running by a docker-compose with
+visual studio code
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,5 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-// dummy startup file, to be replaced by a call to the transpiled typescript start file
-
 import { Request, Response } from 'express';
-import express from "express";
+import express from 'express';
 
 // Constants
 const PORT = 8080;
@@ -15,9 +8,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello remote world!\n');
+  res.send('this is the best way to develop in nodejs\n');
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-console.log("And another line, for testing!");
